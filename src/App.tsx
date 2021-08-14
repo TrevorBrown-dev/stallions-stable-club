b7e70295f06fddca42d487bc47314b3051d2b0bf
Trevor Brown <trevorbrown.dev@gmail.com>
2021-08-14 14:27:23 -0400

    Testing some color pallettes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Contact } from './pages/Contact';
 import { Home } from './pages/Home';
+import { Testing } from './pages/Testing';
 import './styles/style.css';
 export const App: React.FC = () => {
     return (
@@ -10,9 +11,17 @@ export const App: React.FC = () => {
             <Route path='/' exact>
                 <Home />
             </Route>
-            <Route path='/contact' exact>
-                <Contact />
-            </Route>
+            <div className="page-main-container">
+                <div className="page-main">
+                    <Route path='/contact' exact>
+                        <Contact />
+                    </Route>
+                    <Route path='/testing' exact>
+
+                        <Testing />
+                    </Route>
+                </div>
+            </div>
         </Router>
     );
 };
---
+++ src/pages/Testing/index.tsx
@@ -0,0 +1,9 @@
+export const Testing: React.FC = () => {
+    return (
+        <div className="center-content">
+            <h1>Testing inputs</h1>
+
+            <input type="text" />
+        </div>
+    );
+}
