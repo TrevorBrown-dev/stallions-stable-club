a8dbebf0422a86579d8319fb30d8b6561fde201f
Trevor Brown <trevorbrown-dev@github.com>
2021-08-23 19:57:55 -0400

    Added title to button

--- src/components/Footer/ScrollToTop.tsx
+++ src/components/Footer/ScrollToTop.tsx
@@ -3,7 +3,7 @@ export const ScrollToTop: React.FC = () => {
         window.scrollTo({ top: 0 });
     };
     return (
-        <button className='scroll-to-top' onClick={() => handleButtonClick()}>
+        <button className='scroll-to-top' title='Scroll to Top' onClick={() => handleButtonClick()}>
             <span className='material-icons'>keyboard_double_arrow_up</span>
         </button>
     );
